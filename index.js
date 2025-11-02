8335a1f6da541406e20a8ec4a4d108ecf2e50839
luan-bin <nguyenminhluan2742011@gmail.com>
2025-11-02 11:53:47 +0700

    new

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("xinh chào");
+
+let fullname = "Minh Luân"
+
+let fristName = "Minh Luan"
+let lastName =  "Nguyên"
+let email = "email"
+let createAt 
+let updateAt
+
+let str1 = "hello"
+let str2 = 'words'
+let result = str1 + str2;
+console.log(result); // "hello words"
+
+let str = "hello words!!!"
+console.log('số kí tự trong chuổi là: ', str.length); //16
+
+
+document.getElementById("myBtn").addEventListener("click", function() {
+  document.getElementById("text").textContent = "Bạn vừa bấm nút!";
+});
+
